a40a27ec322a602ac6b3cd1e5460d2b634f89833
ryuk@edyrrg <edyrojas64@gmail.com>
2022-10-10 20:54:01 -0600

    update method implement

--- src/handlers/todos.js
+++ src/handlers/todos.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import { getDBHandler } from "../db/index.js"
 
 const toDosReqHandler = express.Router();
@@ -41,7 +41,7 @@ toDosReqHandler.get('/to-dos', async (request, response) => {
         response.send({ todos })
     } catch (error) {
         response.status(500).send({
-            error: `Something went wrong when trying to create a new to do`,
+            error: `Something went wrong when trying to gets to dos`,
             errorInfo: error.message
         })
     }
@@ -60,10 +60,30 @@ toDosReqHandler.delete('/to-dos/:id', async (request, response) => {
 
         await dbHandler.close();
 
-        response.send({toDoRemoved: {... deletedToDo}})
+        response.send({ toDoRemoved: { ...deletedToDo } })
     } catch (error) {
         response.status(500).send({
-            error: `Something went wrong when trying to create a new to do`,
+            error: `Something went wrong when trying to remove a to do`,
+            errorInfo: error.message
+        })
+    }
+})
+
+toDosReqHandler.patch('/to-dos', async (request, response) => {
+    try {
+        const { id, title, description, isDone: is_done } = request.body;
+        const dbHandler = await getDBHandler()
+
+        const updateToDo = dbHandler.run(`
+            UPDATE todos SET title = '${title}', description = '${description}', is_done = ${is_done} WHERE id = ?`,
+            id)
+
+        dbHandler.close()
+
+        response.send({ updateToDo: { id, title, description, is_done, ...updateToDo } })
+    } catch (error) {
+        response.status(500).send({
+            error: `Something went wrong when trying to update a to do`,
             errorInfo: error.message
         })
     }
